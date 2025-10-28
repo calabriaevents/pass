533f5ca97eb261f1c2ed022aeb2b8b679da97156
calabriaevents <info@caservices.it>
2025-10-28 08:08:00 +0100

    1

--- create_image.php
+++
@@ -1,7 +0,0 @@
-<?php
-$image = imagecreatetruecolor(100, 100);
-$red = imagecolorallocate($image, 255, 0, 0);
-imagefill($image, 0, 0, $red);
-imagepng($image, 'test_image.png');
-imagedestroy($image);
-echo "Test image created successfully.";
